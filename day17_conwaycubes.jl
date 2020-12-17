1c921d14d0a1914cbdb4c742ff436c1ffd920887
Ricardo Rosa <rmsrosa@gmail.com>
2020-12-17 16:55:21 -0300

    remove resetting sumxyz and rename it in 4d

--- day17_conwaycubes.jl
+++ day17_conwaycubes.jl
@@ -84,26 +84,70 @@ julia> @btime cycle3d(list,6)
 
 julia> @btime cycle4d(list,6)
   39.148 ms (283334 allocations: 31.09 MiB)
-
 ```
  =#
 
 function cycle3d_onepass(list, cycles)
     m = length(list)
     width = m+2*cycles+2
-    depth = 1+2*cycles+2
+    depth = 3+2*cycles
     space = fill(false, width, width, depth) # m + 2*cycles (might grow once on both sides at each cycle) + 2 (padding)
     for i in 1:m
         space[1+cycles+i, 2+cycles:m+1+cycles, 2+cycles] = map(==('#'), collect(list[i]))
     end
+    sumxyz = fill(0, width, width, depth)
     for cycle = 1:cycles
-        sumxyz = fill(0, width, width, depth)
         sumxyz[:,:,2:end-1] = space[:,:,3:end] + space[:,:,2:end-1] + space[:,:,1:end-2]
         sumxyz[:,2:end-1,:] = sumxyz[:,3:end,:] + sumxyz[:,2:end-1,:] + sumxyz[:,1:end-2,:]
         sumxyz[2:end-1,:,:] = sumxyz[3:end,:,:] + sumxyz[2:end-1,:,:] + sumxyz[1:end-2,:,:]
+        space = map(x -> (x[1] && x[2] ∉ [3,4]) ? false : (!x[1] && x[2] == 3) ? true : x[1], zip(space, sumxyz))  
+    end
+    return count(space)
+end
+
+function cycle4d_onepass(list, cycles)
+    m = length(list)
+    width = m+2*cycles+2
+    depth = 3+2*cycles
+    space = fill(false, width, width, depth, depth)
+    for i in 1:m
+        space[1+cycles+i, 2+cycles:m+1+cycles, 2+cycles, 2+cycles] = 
+            map(==('#'), collect(list[i]))
+    end
+    sumxyz = fill(0, width, width, depth, depth)
+    for cycle = 1:cycles        
+        sumxyz[:,:,:,2:end-1] = space[:,:,:,3:end] + space[:,:,:,2:end-1] + space[:,:,:,1:end-2]
+        sumxyz[:,:,2:end-1,:] = sumxyz[:,:,3:end,:] + sumxyz[:,:,2:end-1,:] + sumxyz[:,:,1:end-2,:]
+        sumxyz[:,2:end-1,:,:] = sumxyz[:,3:end,:,:] + sumxyz[:,2:end-1,:,:] + sumxyz[:,1:end-2,:,:]
+        sumxyz[2:end-1,:,:,:] = sumxyz[3:end,:,:,:] + sumxyz[2:end-1,:,:,:] + sumxyz[1:end-2,:,:,:]
         space = map(x -> (x[1] && x[2] ∉ [3,4]) ? false : (!x[1] && x[2] == 3) ? true : x[1], zip(space, sumxyz))
     end
     return count(space)
 end
 
 @show cycle3d_onepass(list, 6) == 310
+@show cycle4d_onepass(list, 6) == 2056
+
+#= 
+#
+# It turns out the nested loop is still faster because I only consider the points
+# in space next to points which are possibly active. I mean, the range of the loops 
+# grows with the number of cycles, so I minimize the number of neighborhood counts.
+# In the one-pass code, I end up counting everything. Of course I can modify the 
+# code for the range of the sum to grow with the cycles as well, but it is enough
+# for the moment.
+
+```julia
+julia> @btime cycle3d(list, 6);
+  1.802 ms (14266 allocations: 1.57 MiB)
+
+julia> @btime cycle4d(list, 6);
+  40.654 ms (288967 allocations: 31.61 MiB)
+
+julia> @btime cycle3d_onepass(list, 6);
+  2.482 ms (921 allocations: 3.00 MiB)
+
+julia> @btime cycle4d_onepass(list, 6);
+  63.279 ms (5986 allocations: 61.57 MiB)
+``` 
+=#
