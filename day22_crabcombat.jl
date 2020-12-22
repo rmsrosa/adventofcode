1a6725a2f76527617c9aba5e0f97f854fe950741
Ricardo Rosa <rmsrosa@gmail.com>
2020-12-22 18:57:15 -0300

    day22 part2

--- day22_crabcombat.jl
+++ day22_crabcombat.jl
@@ -26,11 +26,11 @@ function play_combat(list)
     N = num_cards
     while 0 < N < 2*num_cards
         cards = decks[:, 1]
+        decks[:, 1:2*num_cards-1] = decks[:, 2:end]
         winner = argmax(cards)
         slot = winner == 1 ? N : 2*num_cards-N
         N += winner == 1 ? 1 : -1
-        decks[:, 1:2*num_cards-1] = decks[:, 2:end]
-        decks[winner, slot:slot+1] = sort(cards, rev=true)
+        decks[winner, slot:slot+1] = [cards[winner] cards[3-winner]]
     end
 
     winner = N == 0 ? 2 : 1
@@ -41,3 +41,54 @@ end
 
 @show play_combat(test_list) == 306
 @show play_combat(list) == 32495
+
+encode(deck) = sum([2^(j-1)*deck[j] for j in 1:length(deck)])
+
+function play_recursive_combat(list)
+    num_cards = div(length(list)+1, 2) - 2
+    decks = fill(0, 2, 2*num_cards)
+    decks[1, 1:num_cards] = parse.(Int, list[2:num_cards+1])
+    decks[2, 1:num_cards] = parse.(Int, list[num_cards+4:end])
+
+    function playgame(decks)
+        num_cards = div(size(decks)[2], 2)
+        history1 = Set()
+        history2 = Set()
+        gamerepeated = false
+        while minimum(sum(decks, dims=2)) > 0 && !gamerepeated
+            encoded_deck1 = encode(decks[1,:])
+            encoded_deck2 = encode(decks[2,:])
+            if  decks[1,:] ∈ history1 || decks[2,:] ∈ history2
+                gamerepeated = true
+                winner = 1
+            else
+                push!(history1, decks[1,:])
+                push!(history2, decks[2,:])
+                cards = decks[:, 1]
+                decks[:, 1:2*num_cards-1] = decks[:, 2:end]
+                if sum(sum(decks.>0, dims=2) .≥ cards) == 2
+                    new_decks = copy(decks)
+                    new_decks[1,cards[1]+1:end] .= 0
+                    new_decks[2,cards[2]+1:end] .= 0
+                    winner = playgame(new_decks)
+                else
+                    winner = argmax(cards)
+                end
+                slot = findfirst(==(0), decks[winner,:])
+                decks[winner, slot:slot+1] = [cards[winner] cards[3-winner]]
+            end
+        end
+        return winner
+    end
+    
+    winner = playgame(decks)
+
+    points = sum([decks[winner, j]*(2*num_cards-j+1) for j in 1:2*num_cards])
+    return points
+end
+
+@show play_recursive_combat(test_list) == 291
+@show play_recursive_combat(list) == 32665
+
+nothing
+
