fafad306d027aa89fd12d01bc56c243ff49cc049
Ricardo Rosa <rmsrosa@gmail.com>
2021-12-04 08:21:30 -0300

    add template

---
+++ aoc2021/day_template.jl
@@ -0,0 +1,28 @@
+list_test_str = split(
+"",
+'\n'
+)
+
+list_str = readlines("day_input.txt")
+
+# Part 1
+
+function part1(list)
+end
+
+@info "Part 1"
+@info "test:"
+@show part1(list_test_str) !== nothing
+@info "challenge:" 
+@show part1(list_str) !== nothing
+
+# Part 2
+
+function part2(list)
+end
+
+@info "Part 2"
+@info "test:"
+@show part2(list_test_str) !== nothing
+@info "challenge:" 
+@show part2(list_str) !== nothing
