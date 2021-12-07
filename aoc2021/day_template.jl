2fc991cff29da5b69261d6f1c715149fbec0c4f1
Ricardo Rosa <rmsrosa@gmail.com>
2021-12-07 08:54:55 -0300

    update template

--- aoc2021/day_template.jl
+++ aoc2021/day_template.jl
@@ -3,13 +3,13 @@ list_test_str = split(
 '\n'
 )
 
-let input = "day_input.txt"
-    isfile(input) && (list_str = readlines(input))
+let input = "day7_input.txt"
+    list_str = isfile(input) ? readlines(input) : list_test_str
 end
 
 # Test function
 
-function testit(name, result, check = (args...) -> false)
+function testit(name, result, check::Function)
     if check(result)
         @info "$name successful, with result $result"
     else
@@ -22,14 +22,14 @@ end
 function part1(list)
 end
 
-testit("Test Part 1", part1(list_test_str), ==(0))
-testit("Challenge Part 1", part1(list_str), ==(0))
+testit("Test Part 1", part1(list_test_str), ==(nothing))
+testit("Challenge Part 1", part1(list_str), ==(nothing))
 
 # Part 2
 
 function part2(list)
 end
 
-testit("Test Part 2", part2(list_test_str), ==(0))
-testit("Challenge Part 2", part2(list_str), ==(0))
+testit("Test Part 2", part2(list_test_str), ==(nothing))
+testit("Challenge Part 2", part2(list_str), ==(nothing))
 
