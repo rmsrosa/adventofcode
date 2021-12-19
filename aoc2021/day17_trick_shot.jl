a923cdfb8a1edaa575723ea9d4e99698851109ae
Ricardo Rosa <rmsrosa@gmail.com>
2021-12-18 21:40:14 -0300

    separate parts

--- aoc2021/day17_trick_shot.jl
+++ aoc2021/day17_trick_shot.jl
@@ -12,13 +12,23 @@ function testit(name, result, check::Function)
     end
 end
 
-# Solving both at the same time
+# Part 1
 
-function solve(list)
+function part1(list)
+    c = parse(Int, match(r"y=(-?\d*)", list).captures[1])
+    return div(c * (c + 1), 2)
+end
+
+testit("Test Part 1", part1(list_test_str), ==(45))
+testit("Challenge Part 1", part1(list_str), ==(5995))
+
+# Part 2
+
+function part2(list)
     a, b, c, d = parse.(Int, match(r"(-?\d*)\.\.(-?\d*), y=(-?\d*)\.\.(-?\d*)", list).captures)
     good = Matrix{Int}(undef, 0, 3)
     for vx in 0:b
-        for vy in c:-2c
+        for vy in c:-c
             xmax = div(vx * (vx + 1), 2)
             y = 0
             n = 0
@@ -37,12 +47,5 @@ function solve(list)
     return good, size(unique(good[:, 1:2], dims=1), 1), maximum(goodmaxima)
 end
 
-# Part 1
-
-testit("Test Part 1", solve(list_test_str)[3], ==(45))
-testit("Challenge Part 1", solve(list_str)[3], ==(5995))
-
-# Part 2
-
-testit("Test Part 2", solve(list_test_str)[2], ==(112))
-testit("Challenge Part 2", solve(list_str)[2], ==(3202))
+testit("Test Part 2", part2(list_test_str)[2], ==(112))
+testit("Challenge Part 2", part2(list_str)[2], ==(3202))
